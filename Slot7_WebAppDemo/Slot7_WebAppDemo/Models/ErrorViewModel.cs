66e7cc5758cb76312e6eff7aa8269e3d3b4e2eb3
Pham Thi Thuy Linh <thuylinhcua2024@gmail.com>
2025-06-09 08:13:15 +0700

    Slot 7 - CV

---
+++ Slot7_WebAppDemo/Slot7_WebAppDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Slot7_WebAppDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
